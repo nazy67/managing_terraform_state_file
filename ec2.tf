eb398672e8a1da9208d7180582c179c9ded1adc1
nazy67 <nazykh67@gmail.com>
2021-05-30 22:30:16 -0500

    modified ec2.tf, added secirity group id

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "first_ec2" {
   depends_on             = [ aws_security_group.sg_for_ec2 ]
   ami                    = "ami-0be2609ba883822ec"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.sg_for_ec2.id ]
+  vpc_security_group_ids = [aws_security_group.sg_for_ec2.id]
   tags = {
     Name        = "webserver_1"
     Environment = var.env 
@@ -13,7 +13,7 @@ resource "aws_instance" "second_ec2" {
   depends_on             = [ aws_security_group.sg_for_ec2 ]
   ami                    = "ami-0be2609ba883822ec"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.sg_for_ec2 ]
+  vpc_security_group_ids = [ aws_security_group.sg_for_ec2.id ]
   tags = {
     Name        = "webserver_2"
     Environment = var.env 
