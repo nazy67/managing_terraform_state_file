f884524d1c92c7a30e9e0d6ca73b8164e323423e
nazy67 <nazykh67@gmail.com>
2021-05-30 22:33:33 -0500

    formatted ec2.tf, providers.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "first_ec2" {
   vpc_security_group_ids = [aws_security_group.sg_for_ec2.id]
   tags = {
     Name        = "webserver_1"
-    Environment = var.env 
+    Environment = var.env
   }
 }
 
@@ -16,6 +16,6 @@ resource "aws_instance" "second_ec2" {
   vpc_security_group_ids = [aws_security_group.sg_for_ec2.id]
   tags = {
     Name        = "webserver_2"
-    Environment = var.env 
+    Environment = var.env
   }
 }
