60d0d69ba28da9bb09b88366f158f65c2eef5bd7
nazy67 <nazykh67@gmail.com>
2021-06-01 18:58:15 -0500

    modified ec2.tf, added new_ec2 resource

--- ec2.tf
+++ ec2.tf
@@ -19,3 +19,14 @@ resource "aws_instance" "second_ec2" {
     Environment = var.env
   }
 }
+
+resource "aws_instance" "new_ec2" {
+  depends_on             = [aws_security_group.sg_for_ec2]
+  ami                    = "ami-0be2609ba883822ec"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg_for_ec2.id]
+  tags = {
+    Name        = "webserver_3"
+    Environment = var.env
+  }
+}
